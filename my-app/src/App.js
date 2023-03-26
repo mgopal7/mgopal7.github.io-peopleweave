19adb9942c957b64dad2f88d821edd31a8bad1ff
mgopal7 <mgopal7@illinois.edu>
2023-03-26 18:55:21 -0500

    fixes using github pages
    adds github pages functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import IntroductionPage from "./IntroductionPage";
-import DataPage from "./DataPage";
-import ContactPage from "./ContactPage";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import IntroductionPage from './IntroductionPage';
+import DataPage from './DataPage';
+import ContactPage from './ContactPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Header />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Introduction</Link>
+            </li>
+            <li>
+              <Link to="/data">Data</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
         <Switch>
-          <Route exact path="/" render={() => <IntroductionPage />} />
-          <Route exact path="/data" render={() => <DataPage />} />
-          <Route exact path="/contact" render={() => <ContactPage />} />
+          <Route exact path="/" component={IntroductionPage} />
+          <Route path="/data" component={DataPage} />
+          <Route path="/contact" component={ContactPage} />
         </Switch>
       </Router>
     </ChakraProvider>
