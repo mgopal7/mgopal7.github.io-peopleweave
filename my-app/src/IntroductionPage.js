f9b7126a9e33eca9e15f676c3e53b19d867ed8b8
mgopal7 <mgopal7@illinois.edu>
2023-03-26 21:31:23 -0500

    updates
    some more updates

--- my-app/src/IntroductionPage.js
+++ my-app/src/IntroductionPage.js
@@ -6,7 +6,11 @@ function IntroductionPage() {
       <Heading as="h2" size="xl" mb="4">Welcome to My Website!</Heading>
 
       <Box mb="8">
-        <Text fontSize="xl" fontWeight="bold" mb="4">I am interested in PeopleWeave because I want to do research</Text>
+        <Text fontSize="xl" fontWeight="bold" mb="4">As a web developer, I am excited about working on the PeopleWeave project because it offers an opportunity to use my skills to make a meaningful impact in the world. PeopleWeave's mission to empower underserved communities through technology aligns with my personal values and inspires me to create solutions that can make a positive difference in people's lives. I believe that contributing to this project will not only enhance my technical abilities but also give me a sense of purpose and fulfillment in my work.
+
+
+
+</Text>
       </Box>
 
       <Box mb="8">
